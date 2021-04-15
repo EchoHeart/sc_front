973dbf9ede2948cd3fca71bf033435a978161141
EchoHeart <2931473049@qq.com>
2021-04-15 09:02:25 +0800

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,171 @@
+<template>
+  <body id="home">
+    <!--引入container布局-->
+    <el-container class="home-container">
+      <!--头部-->
+      <el-header>
+        <!--logo + title-->
+        <div style="display: flex; align-items: center; margin-left: 5px">
+          <img src="../assets/logo.jpeg" alt class="logo">
+          <span style="margin-left: 15px">安全圈后台管理系统</span>
+        </div>
+        <!--用户下拉菜单-->
+        <div style="align-items: center; margin-top: 10px">
+          <el-dropdown>
+          <span class="el-dropdown-link" style="margin-right: 30px" >
+            欢迎使用
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>
+                <el-link @click="" icon="el-icon-user-solid">修改个人信息</el-link>
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <el-link @click="exit" icon="el-icon-switch-button">退出</el-link>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-header>
+      <el-container>
+        <!--侧边栏-->
+        <el-aside width="200px">
+          <el-menu background-color="#545c64" text-color="#fff" style="width: 200px" active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-setting"></i>
+                <span>信息管理</span>
+              </template>
+              <el-menu-item index="1-1" class="el-icon-user" v-bind:disabled="this.disable_1" @click="userManage">账户管理</el-menu-item>
+              <el-menu-item index="1-2" class="el-icon-school" v-bind:disabled="this.disable_2" @click="schoolManage">学校管理</el-menu-item>
+              <el-menu-item index="1-3" class="el-icon-check" v-bind:disabled="this.disable_3" @click="check">资质审核</el-menu-item>
+              <el-submenu index="1-4">
+                <template slot="title">
+                  <i class="el-icon-food"></i>
+                  <span style="margin-left: -8px">套餐管理</span>
+                </template>
+                <el-menu-item index="1-4-1" v-bind:disabled="this.disable_4" class="el-icon-shopping-cart-2">套餐购买</el-menu-item>
+                <el-menu-item index="1-4-1" v-bind:disabled="this.disable_5" class="el-icon-setting">套餐设置</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-reading"></i>
+                <span>教学管理</span>
+              </template>
+              <el-menu-item index="2-1" v-bind:disabled="this.disable_6" class="el-icon-document-copy">题库管理</el-menu-item>
+              <el-menu-item index="2-2" v-bind:disabled="this.disable_7" class="el-icon-menu">安排课程</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-s-data"></i>
+                <span>数据图表</span>
+              </template>
+              <el-menu-item index="3-1" v-bind:disabled="this.disable_8" class="iconfont icon-icon-test">地震</el-menu-item>
+              <el-menu-item index="3-3" v-bind:disabled="this.disable_9" class="iconfont icon-zhongdahuozai">火灾</el-menu-item>
+              <el-menu-item index="3-2" v-bind:disabled="this.disable_10" class="iconfont icon-ic_shuiliu">泥石流</el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <!--主体-->
+        <el-main>
+          Main
+        </el-main>
+      </el-container>
+    </el-container>
+  </body>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      Identity: '',
+      disable_1: false,
+      disable_2: false,
+      disable_3: false,
+      disable_4: false,
+      disable_5: false,
+      disable_6: false,
+      disable_7: false,
+      disable_8: false,
+      disable_9: false,
+      disable_10: false
+    }
+  },
+  created() {
+    this.Identity = window.sessionStorage.getItem("Identity");
+    if(this.Identity == "admin"){
+      this.disable_4 = true;
+      this.disable_6 = true;
+      this.disable_7 = true;
+      this.disable_8 = true;
+      this.disable_9 = true;
+      this.disable_10 = true;
+    }
+    if(this.Identity == "headmaster"){
+      this.disable_5 = true;
+      this.disable_6 = true;
+      this.disable_7 = true;
+    }
+    if(this.Identity == "teacher"){
+      this.disable_1 = true;
+      this.disable_2 = true;
+      this.disable_3 = true;
+      this.disable_4 = true;
+      this.disable_5 = true;
+    }
+  },
+  name: "Home",
+  methods: {
+    /**
+     * function: 安全退出
+     * description: 用户退出时，清空用户所有信息
+     */
+    exit(){
+      window.sessionStorage.clear();
+      // 返回登录页面
+      this.$router.push("/");
+    },
+    userManage(){
+
+    },
+    schoolManage(){
+
+    },
+    check(){
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-header{
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0px;
+  font-size: 20px;
+  font-weight: bolder;
+}
+.el-aside{
+  background-color: #545c64;
+}
+.el-main{
+  background-color: #eaedf1;
+}
+.home-container{
+  height: 100%;
+}
+.logo{
+  width: 55px;
+  height: 55px;
+}
+#home {
+  height: 100%;
+  width: 100%;
+  margin: -8px 20px 30px -8px;
+  background-size: cover;
+  position: fixed;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,222 @@
+<template>
+  <body id="poster">
+  <div>
+    <!--登录表单-->
+    <el-form :rules="rules_login" ref="loginForm" :model="loginForm" class="loginContainer">
+      <!--标题-->
+      <h3 class="loginTitle">欢迎使用安全圈后台管理系统</h3>
+      <!--用户名-->
+      <el-form-item prop="username">
+        <el-input type="text" auto-complete="false" v-model="loginForm.username"
+                  placeholder="请输入用户名" suffix-icon="iconfont icon-denglu-copy"></el-input>
+      </el-form-item>
+      <!--密码-->
+      <el-form-item prop="password">
+        <el-input type="password" auto-complete="false" v-model="loginForm.password"
+                  placeholder="请输入密码" suffix-icon="iconfont icon-icon-mima"></el-input>
+      </el-form-item>
+      <!--登录按钮-->
+      <el-form-item align="center">
+        <el-button type="info" icon="iconfont icon-jiaolian1" @click="teacherLogin">老师</el-button>
+        <el-button type="info" icon="iconfont icon-Boss-laoban-1" @click="headmasterLogin">校长</el-button>
+        <el-button type="info" icon="iconfont icon-guanliyuan" @click="adminLogin">管理员</el-button>
+      </el-form-item>
+      <!--注册-->
+      <el-form-item align="center">
+        <el-link :underline="false" icon="iconfont icon-jiaolian1" type="primary"
+                 style="margin-right: 30px" @click="Visible=true">老师注册</el-link>
+        <el-link :underline="false" icon="iconfont icon-Boss-laoban-1" type="primary"
+                 @click="Visible=true">校长注册</el-link>
+      </el-form-item>
+      <!--密码找回-->
+      <el-link :underline="false" href="" icon="iconfont icon-wangjimima" type="warning"
+               style="margin-left: 50%; transform: translate(-50%)">忘记密码</el-link>
+    </el-form>
+    <!--注册表单-->
+    <el-dialog title="用户注册" :visible.sync="Visible" :append-to-body="true">
+      <el-form :model="registerForm" ref="registerForm" :rules="rules_register">
+        <el-form-item label="用户名称" :label-width="formLabelWidth" prop="register_username">
+          <el-input v-model="registerForm.register_username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="用户密码" :label-width="formLabelWidth" prop="register_password">
+          <el-input type="password" v-model="registerForm.register_password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话号码" :label-width="formLabelWidth" prop="register_telephone">
+          <el-input v-model="registerForm.register_telephone" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="所属学校" :label-width="formLabelWidth" prop="register_school">
+          <el-input v-model="registerForm.register_school" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="cancel">取 消</el-button>
+        <el-button type="primary" @click="register">注 册</el-button>
+      </div>
+    </el-dialog>
+  </div>
+  </body>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{
+      Visible: false,
+      formLabelWidth: '100px',
+      loginForm:{
+        username:'',
+        password:''
+      },
+      registerForm: {
+        register_username: '',
+        register_password: '',
+        register_telephone: '',
+        register_school: ''
+      },
+      /**
+       * 表单验证
+       * required: 是否为必填项
+       * message: 提示信息
+       * trigger: 触发器
+       */
+      rules_login:{
+        //用户名不能为空
+        username: [{required:true, message:'请输入用户名', trigger:'blur'}],
+        //密码不能为空
+        password: [{required:true, message:'请输入密码', trigger:'blur'}]
+      },
+      rules_register:{
+        //用户名称不能为空
+        register_username: [{required:true, message:'请输入用户名', trigger:'blur'}],
+        //用户密码不能为空
+        register_password: [{required:true, message:'请输入密码', trigger:'blur'}],
+        //电话号码不能为空
+        register_telephone: [{required:true, message:'请输入密码', trigger:'blur'}],
+        //所属学校不能为空
+        register_school: [{required:true, message:'请输入密码', trigger:'blur'}],
+      }
+    }
+  },
+  methods:{
+    adminLogin(){
+      //valid用来判断表单规则是否通过
+      this.$refs.loginForm.validate(async (valid) => {
+        if (valid) {
+          const {data: res} = await this.$http.post("adminLogin", {
+            admin_name: this.loginForm.username,
+            password: this.loginForm.password
+          });
+          if(res.flag == "ok"){
+            window.sessionStorage.setItem("user",res.object);//存储admin对象
+            window.sessionStorage.setItem("Identity","admin");
+            this.$message.success("登录成功");
+            await this.$router.push({path: "/Home"});
+          }else{
+            this.$message.error("用户名或密码错误");
+            //重置表单
+            this.$refs.loginForm.resetFields();
+          }
+        } else {
+          this.$message.error('用户名或密码不能为空！');
+          return false;
+        }
+      });
+    },
+    teacherLogin(){
+      //valid用来判断表单规则是否通过
+      this.$refs.loginForm.validate(async (valid) => {
+        if (valid) {
+          const {data: res} = await this.$http.post("teacherLogin", {
+            teacher_name: this.loginForm.username,
+            password: this.loginForm.password
+          });
+          if(res.flag == "ok"){
+            window.sessionStorage.setItem("user",res.object);//存储teacher对象
+            window.sessionStorage.setItem("Identity","teacher");
+            this.$message.success("登录成功");
+            await this.$router.push({path: "/Home"});
+          }else{
+            this.$message.error("用户名或密码错误");
+            //重置表单
+            this.$refs.loginForm.resetFields();
+          }
+        } else {
+          this.$message.error('用户名或密码不能为空！');
+          return false;
+        }
+      });
+    },
+    headmasterLogin(){
+      //valid用来判断表单规则是否通过
+      this.$refs.loginForm.validate(async (valid) => {
+        if (valid) {
+          const {data: res} = await this.$http.post("headmasterLogin", {
+            headmaster_name: this.loginForm.username,
+            password: this.loginForm.password
+          });
+          if(res.flag == "ok"){
+            window.sessionStorage.setItem("user",res.object);//存储headmaster对象
+            window.sessionStorage.setItem("Identity","headmaster");
+            this.$message.success("登录成功");
+            await this.$router.push({path: "/Home"});
+          }else{
+            this.$message.error("用户名或密码错误");
+            //重置表单
+            this.$refs.loginForm.resetFields();
+          }
+        } else {
+          this.$message.error('用户名或密码不能为空！');
+          return false;
+        }
+      });
+    },
+    cancel(){
+      this.Visible = false;
+      this.$refs.registerForm.resetFields();
+    },
+    register(){
+      this.$refs.registerForm.validate(async (valid) =>{
+        if(valid){
+          //注册
+
+          this.Visible = false;
+          this.$refs.registerForm.resetFields();
+        }else{
+          this.$message.error('个人信息需填写完整！');
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.loginContainer{
+  background-clip: padding-box;
+  background-color: #fff;
+
+  padding: 35px 35px 35px 35px;
+  border-radius: 15px;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+
+  width: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+.loginTitle{
+  margin: 0px auto 15px auto;
+  text-align: center;
+}
+#poster {
+  background: url("../assets/loginBG.jpeg");
+  height: 100%;
+  width: 100%;
+  margin: -8px 20px 30px -8px;
+  background-size: cover;
+  position: fixed;
+}
+</style>
